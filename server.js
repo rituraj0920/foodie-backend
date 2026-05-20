ef9a7d501e61076ac75f79e216bab3cc5a81fc76
rituraj0920 <rituraj0920@gmail.com>
2026-05-20 12:26:25 +0530

    vercel added

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ require('dotenv').config();
 
 
 
-
-
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 })
--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const cors =require("cors");
 const app=express();
 
 app.use(cors({
-    origin:"https://foodscrol-frontend.vercel.app",
+    origin:"http://localhost:5173",
     credentials:true
 }))
 
