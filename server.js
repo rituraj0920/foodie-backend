1378b449558af3c1aa4af2cb0a2d6a28e7e52da7
rituraj0920 <rituraj0920@gmail.com>
2026-05-24 00:16:58 +0530

    testing

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-const app = require("./src/app");
-const connectDB = require("./src/db/db");
+const app =require("./src/app");
+const connectDB =require("./src/db/db");
+connectDB();
 require('dotenv').config();
 
-// Connect to the database
-connectDB();
 
-// Export the app for Vercel's serverless environment
-module.exports = app;
 
-// Only start the listener if we are NOT in Vercel's production environment
-if (process.env.NODE_ENV !== "production") {
-    app.listen(3000, () => {
-        console.log("server is running on port 3000");
-    });
-}
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
